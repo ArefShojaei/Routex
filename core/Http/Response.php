ad04dfc6f87107329a99d1f85545e7c4a3461daa
ArefShojaei <arefshojaei82@gmail.com>
2026-05-14 13:27:00 +0330

    chore: add HTTP status code

--- core/Http/Response.php
+++ core/Http/Response.php
@@ -5,6 +5,36 @@ namespace Routex\Http;
 use Routex\Contracts\Response as IResponse;
 
 final class Response implements IResponse {
+    # HTTP Success code
+    public const HTTP_OK = 200;
+    public const HTTP_CREATED = 201;
+    public const HTTP_NO_CONTENT = 204;
+
+    # HTTP Redirection code
+    public const HTTP_MOVED_PERMANENTLY = 301;
+    public const HTTP_FOUND = 302;
+    public const HTTP_SEE_OTHER = 303;
+    public const HTTP_NOT_MODIFIED = 304;
+    public const HTTP_TEMPORARY_REDIRECT = 307;
+    public const HTTP_PERMANENT_REDIRECT = 308;
+
+    # HTTP Client code
+    public const HTTP_BAD_REQUEST = 400;
+    public const HTTP_UNAUTHORIZED = 401;
+    public const HTTP_FORBIDDEN = 403;
+    public const HTTP_NOT_FOUND = 404;
+    public const HTTP_METHOD_NOT_ALLOWED = 405;
+    public const HTTP_CONFLICT = 409;
+    public const HTTP_UNPROCESSABLE_ENTITY = 422;
+    public const HTTP_TOO_MANY_REQUESTS = 429;
+
+    # HTTP Server code
+    public const HTTP_INTERNAL_SERVER_ERROR = 500;
+    public const HTTP_BAD_GATEWAY = 502;
+    public const HTTP_SERVICE_UNAVAILABLE = 503;
+    public const HTTP_GATEWAY_TIMEOUT = 504;
+
+
     public function status(int $code): self {
         http_response_code($code);
         
