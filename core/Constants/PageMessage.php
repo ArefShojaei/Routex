fd2d4646c7ca62a360844bf8fb3cb26349829eda
ArefShojaei <arefshojaei82@gmail.com>
2026-05-16 12:46:04 +0330

    chore: add new page message constants

--- core/Constants/PageMessage.php
+++ core/Constants/PageMessage.php
@@ -5,4 +5,8 @@ namespace Routex\Constants;
 
 final class PageMessage {
     public const NOT_FOUND = "404 | Page not found!";
+    public const UNAUTHORIZED = "401 | Unauthorized access!";
+    public const FORBIDDEN = "403 | Forbidden!";
+    public const INTERNAL_SERVER_ERROR = "500 | Internal server error!";
+    public const BAD_REQUEST = "400 | Bad request!";
 }
